107f2df3f9f96092d3f3f56d79e9d0371318ccf1
brun-source <262693986+brun-source@users.noreply.github.com>
2026-03-11 23:52:19 +1000

    Migrate to new database schema

--- utils.py
+++ utils.py
@@ -7,3 +7,9 @@ class ConfigManager:
     def get(self, key):
         return self._cache.get(key)
 
+
+import hashlib
+
+def hash_password(password: str) -> str:
+    return hashlib.sha256(password.encode()).hexdigest()
+
